e88be52ab0fed19c069b7eff8a761a6a36e8a50a
Tom van der Woerdt <info@tvdw.eu>
2021-03-24 17:35:07 +0100

    Remove code that forbids downgrades
    Fits are now checked manually, and this code creates the occasional
    strange result.

--- api/waitlist/tdf/modules.py
+++ api/waitlist/tdf/modules.py
@@ -6,7 +6,6 @@ from ..data.evedb import id_of, type_variations
 def _compute_alternatives(
     destination: Dict[int, List[Tuple[int, int]]],
     source: List[List[List[str]]],
-    allow_downgrade: bool,
 ) -> None:
     for group in source:
         this_group: List[Tuple[int, int]] = []
@@ -18,30 +17,19 @@ def _compute_alternatives(
         for module_i, tier_i in this_group:
             destination.setdefault(module_i, [])
             for module_j, tier_j in this_group:
-                if tier_j >= tier_i or allow_downgrade:
-                    destination[module_i].append((module_j, tier_j - tier_i))
+                destination[module_i].append((module_j, tier_j - tier_i))
 
 
 def _from_meta(
     destination: Dict[int, List[Tuple[int, int]]],
     meta_items: List[str],
-    allow_downgrade: bool,
 ) -> None:
     for item in meta_items:
         meta_levels = type_variations(id_of(item))
-        base_meta = meta_levels[id_of(item)]
         for module_i, meta_i in meta_levels.items():
-            if meta_i < base_meta:
-                continue
-
             destination.setdefault(module_i, [])
             for module_j, meta_j in meta_levels.items():
-                if meta_j < base_meta:
-                    continue
-
-                is_downgrade = meta_j < meta_i
-                if allow_downgrade or not is_downgrade:
-                    destination[module_i].append((module_j, meta_j - meta_i))
+                destination[module_i].append((module_j, meta_j - meta_i))
 
 
 def _add_t1(destination: Dict[int, List[Tuple[int, int]]], t2_items: List[str]) -> None:
@@ -62,13 +50,18 @@ def load_alternatives() -> Dict[int, List[Tuple[int, int]]]:
     alternatives: Dict[int, List[Tuple[int, int]]] = {}
 
     # Generate all possible valid permutations for the alternatives listed in the data
-    _compute_alternatives(alternatives, modules_raw["alternatives"], True)
-    _compute_alternatives(alternatives, modules_raw["no_downgrade"], False)
-
-    _from_meta(alternatives, modules_raw["from_meta"], True)
-    _from_meta(alternatives, modules_raw["from_meta_no_downgrade"], False)
+    _compute_alternatives(alternatives, modules_raw["alternatives"])
+    _from_meta(alternatives, modules_raw["from_meta"])
 
     _add_t1(alternatives, modules_raw["accept_t1"])
+
+    # Sort all lists by distance from the base module
+    for (
+        module_id
+    ) in alternatives.keys():  # pylint: disable=consider-iterating-dictionary
+        alternatives[module_id] = sorted(
+            alternatives[module_id], key=lambda alt: abs(alt[1])
+        )
 
     return alternatives
 
