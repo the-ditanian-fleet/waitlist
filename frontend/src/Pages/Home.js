cfb726de72364c2a0c8e64ae9ac8fc8372020fe5
Tom van der Woerdt <info@tvdw.eu>
2021-07-04 10:35:29 +0700

    Update home page

--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -7,11 +7,11 @@ export function Home() {
       <h2>Welcome to The Ditanian Fleet</h2>
       <p>
         Hello capsuleers, we would like to introduce you to the incursion community called The
-        Ditanian Fleet (TDF). We are a new-bro friendly group where anyone is welcome. We have
+        Ditanian Fleet (TDF). We are a new-bro friendly group where all are welcome. We have
         affordable starter fits, and we can help make you space rich, all we ask in return is you
-        follow our upgrade policy to help the community excel and follow the rules! We exclude any
-        pilot that has previous history of ganking, and we have a no tolerance policy for racism,
-        sexism or anti-social behaviour.
+        follow our upgrade policy to help the community excel and follow the rules! We exclude
+        pilots that have a previous history of ganking, and we have a no tolerance policy for
+        racism, sexism or anti-social behaviour.
       </p>
       <p>
         The Ditanian Fleet is an armor doctrine incursion community. We run Vanguards (10 person),
@@ -21,43 +21,45 @@ export function Home() {
       <p>
         Check out our <NavLink to="/guide">Guides</NavLink> section for all the information you need
         to get started, see <NavLink to="/guide/newbro">new-bro guide</NavLink>,{" "}
-        <NavLink to="/guide/xup">first fleet guide</NavLink> etc, and also join the in-game chat
-        channel <em>TDF-Official</em>, you will find the mailing lists for fits and other usual
+        <NavLink to="/guide/xup">first fleet guide</NavLink> etc, also join the in-game chat channel{" "}
+        <em>TDF-Official</em>, where you will find the mailing lists for fits and other usual
         information in the MOTD (message of the day), we look forward to flying with you!
       </p>
-      <h3>Armor Vs Shields</h3>
-      <p>
-        Unsure about joining an Armor fleet due to your skills being geared towards shield tanking?
-        Here are just some advantages an Armor fleet has over a shield fleet:
-      </p>
-      <ul>
-        <li>
-          Armor fleets are far safer, as your tank is not cap dependent. So, if you get neuted out
-          by an Outuni NPC your tank will not turn off, this means your much safer if you end up in
-          sticky situation. We also use the Nestor battleship which is equal to two conventional
-          tech 2 logistic cruisers and has the ability to allow you to refit your ship on the fly.
-        </li>
-        <li>
-          With our low slots being used for tanking, the mid slots on our ships are free for damage
-          application mods such as webifiers and tracking computers, giving us much better damage
-          application.
-        </li>
-      </ul>
+      <h3>Why Fly TDF?</h3>
+      <li>
+        Armor fleets are far safer, as your tank is passive and thus not cap dependent. If you get
+        neuted your tank will not turn off, this gives you that extra bit of room for error should
+        you need it.
+      </li>
+      <li>
+        With our low slots being used for tanking, the mid slots on our ships are free for damage
+        application mods such as webifiers and tracking computers, giving us great damage
+        application.
+      </li>
+      <li>
+        Our upgrade policy ensures that everyone in the fleet is constantly improving and
+        contributing to that ever-important factor, your ISK per hour.
+      </li>
+      <li>
+        We provide peace of mind with our SRP (ship replacement program), so should you lose your
+        ship due to logi or FC error, you will be reimbursed.
+      </li>
       <h3>What are Incursions?</h3>
       <p>
         Incursions are automated events introduced with the Incursion expansion in which the NPC
         faction known as the Sansha&apos;s Nation, led by Sansha Kuvakei, invade space in an attempt
         to conquer it for themselves. Capsuleers must fight off Sansha&apos;s forces in order to
-        return the contested space back into an area which can be safely occupied.
+        return the contested space back to an area which can be safely occupied.
       </p>
       <p>
-        Incursions are high-end PvE fleet content, well above that of regular level four missions.
-        The introduction of incursions to the game added some much-needed fleet content in highsec
-        space. It also gives people a very controlled environment to train up their skills as a
-        logistics pilot, as well as giving people a feel for organized fleets. Incursion fleets rely
-        on buffers and resists supported by logistics ships in order to survive. The NPCs you face
-        in incursions use electronic warfare, capacitor warfare as well as playing on speed and
-        signature to their advantage.
+        Incursions are high-end PvE fleet content, well above that of regular level four missions,
+        where you can earn upwards of 200mil ISK/h on average. The introduction of incursions to the
+        game added some much-needed fleet content in highsec space. It also gives people a very
+        controlled environment to train up their skills as a logistics pilot, as well as giving
+        people a feel for organized fleets. Incursion fleets rely on buffers and resists supported
+        by logistics ships in order to survive. The NPCs you face in incursions use electronic
+        warfare, capacitor warfare as well as playing on speed and signature to their advantage and
+        this helps to teach you many of the ingame mechanics you may face.
       </p>
     </Content>
   );
