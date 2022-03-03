41c30c76775fed477a2a6f4a485ab4d6c0194f75
Kim Madsen <1686647+kimnnmadsen@users.noreply.github.com>
2022-03-03 23:02:36 +0100

    Update xup.rs
    Delete all exsisting hulls for current character when you Xup. So you cannot Xup as alt and as main.

--- backend/src/routes/waitlist/xup.rs
+++ backend/src/routes/waitlist/xup.rs
@@ -245,18 +245,10 @@ async fn xup_multi(
         let implant_set_id = dedup_implants(&mut tx, this_pilot_data.implants).await?;
 
         // Delete existing X'up for the hull
-        if is_alt {
-            if let Some(existing_x) = sqlx::query!("
-            SELECT waitlist_entry_fit.id FROM waitlist_entry_fit JOIN fitting ON fit_id=fitting.id WHERE character_id = ? AND hull = ? AND is_alt = 1
-            ",character_id, fit.hull).fetch_optional(&mut tx).await? {
-                sqlx::query!("DELETE FROM waitlist_entry_fit WHERE id = ?", existing_x.id).execute(&mut tx).await?;
-            }
-        } else {
-            if let Some(existing_x) = sqlx::query!("
-            SELECT waitlist_entry_fit.id FROM waitlist_entry_fit JOIN fitting ON fit_id=fitting.id WHERE entry_id = ? AND hull = ? AND is_alt = 0
-            ", entry_id, fit.hull).fetch_optional(&mut tx).await? {
+        if let Some(existing_x) = sqlx::query!("
+        SELECT waitlist_entry_fit.id FROM waitlist_entry_fit JOIN fitting ON fit_id=fitting.id WHERE character_id = ? AND hull = ?
+        ",character_id, fit.hull).fetch_optional(&mut tx).await? {
             sqlx::query!("DELETE FROM waitlist_entry_fit WHERE id = ?", existing_x.id).execute(&mut tx).await?;
-            }
         }
 
         let fit_checked = tdf::fitcheck::FitChecker::check(this_pilot_data, &fit)?;
