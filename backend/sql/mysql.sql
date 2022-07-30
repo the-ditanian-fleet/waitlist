8341e6dc2dc9af2e36d959a1d2fe1a37b8d30e85
Luna Duclos <luna.duclos@embark-studios.com>
2022-07-30 20:31:12 +0200

    Syntax error fix

--- backend/sql/mysql.sql
+++ backend/sql/mysql.sql
@@ -104,7 +104,7 @@ CREATE TABLE `announcement` (
   `created_at` bigint NOT NULL,
   PRIMARY KEY (`id`),
   KEY `character_id` (`character_id`),
-  CONSTRAINT `created_by` FOREIGN KEY (`character_id`) REFERENCES `character` (`id`),
+  CONSTRAINT `created_by` FOREIGN KEY (`character_id`) REFERENCES `character` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `skill_current` (
