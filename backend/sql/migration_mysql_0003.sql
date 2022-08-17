de636e4fd7f5db8e64c2cafbc9249a14e21fe346
samuelgrant <samueljegrant@outlook.com>
2022-08-17 21:16:50 +1200

    Migrate badge assignments
    Create badge_assignments based on rows in admins table, then delete non-fc rows from admins.

--- backend/sql/migration_mysql_0003.sql
+++ backend/sql/migration_mysql_0003.sql
@@ -17,3 +17,40 @@ INSERT INTO badge (name) VALUES ('BASTION');
 INSERT INTO badge (name) VALUES ('LOGI');
 INSERT INTO badge (name) VALUES ('RETIRED-LOGI');
 INSERT INTO badge (name) VALUES ('WEB');
+
+# Set vars AdminId, CreatedAt
+SELECT @admin_id := character_id FROM admins WHERE level='admin';
+SELECT @created_at := UNIX_TIMESTAMP();
+
+# Migrate Logi
+INSERT INTO badge_assignment (CharacterId, BadgeId, GrantedById, GrantedAt) SELECT character_id, '2', @admin_id, @created_at FROM admins WHERE admins.level = 'l';
+DELETE FROM admins WHERE admins.level = 'l';
+
+# Migrate Bastion
+INSERT INTO badge_assignment (CharacterId, BadgeId, GrantedById, GrantedAt) SELECT character_id, '1', @admin_id, @created_at FROM admins WHERE admins.level = 'b';
+DELETE FROM admins WHERE admins.level = 'b';
+
+# Migrate Webs
+INSERT INTO badge_assignment (CharacterId, BadgeId, GrantedById, GrantedAt) SELECT character_id, '4', @admin_id, @created_at FROM admins WHERE admins.level = 'w';
+DELETE FROM admins WHERE admins.level = 'w';
+
+# Migrate Logi/Bastion
+INSERT INTO badge_assignment (CharacterId, BadgeId, GrantedById, GrantedAt) SELECT character_id, '1', @admin_id, @created_at FROM admins WHERE admins.level = 'lb';
+INSERT INTO badge_assignment (CharacterId, BadgeId, GrantedById, GrantedAt) SELECT character_id, '2', @admin_id, @created_at FROM admins WHERE admins.level = 'lb';
+DELETE FROM admins WHERE admins.level = 'lb';
+
+# Migrate Logi/Bastion/Webs
+INSERT INTO badge_assignment (CharacterId, BadgeId, GrantedById, GrantedAt) SELECT character_id, '1', @admin_id, @created_at FROM admins WHERE admins.level = 'lbw';
+INSERT INTO badge_assignment (CharacterId, BadgeId, GrantedById, GrantedAt) SELECT character_id, '2', @admin_id, @created_at FROM admins WHERE admins.level = 'lbw';
+INSERT INTO badge_assignment (CharacterId, BadgeId, GrantedById, GrantedAt) SELECT character_id, '4', @admin_id, @created_at FROM admins WHERE admins.level = 'lbw';
+DELETE FROM admins WHERE admins.level = 'lbw';
+
+# Migrate Logi/Webs
+INSERT INTO badge_assignment (CharacterId, BadgeId, GrantedById, GrantedAt) SELECT character_id, '2', @admin_id, @created_at FROM admins WHERE admins.level = 'lw';
+INSERT INTO badge_assignment (CharacterId, BadgeId, GrantedById, GrantedAt) SELECT character_id, '4', @admin_id, @created_at FROM admins WHERE admins.level = 'lw';
+DELETE FROM admins WHERE admins.level = 'lw';
+
+# Migrate Bastion/Webs
+INSERT INTO badge_assignment (CharacterId, BadgeId, GrantedById, GrantedAt) SELECT character_id, '1', @admin_id, @created_at FROM admins WHERE admins.level = 'bw';
+INSERT INTO badge_assignment (CharacterId, BadgeId, GrantedById, GrantedAt) SELECT character_id, '4', @admin_id, @created_at FROM admins WHERE admins.level = 'bw';
+DELETE FROM admins WHERE admins.level = 'bw';
